377c4ba87b7eb231cbf6bbca567b9486b501f616
Piotr Król <piotr.krol@3mdeb.com>
2024-06-10 23:12:00 +0200

    test bashly yaml lib
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/sync_command.sh
+++ src/sync_command.sh
@@ -8,55 +8,66 @@ DEFINITION="${args[--definition]}"
 
 # Function to load repository definitions from a YAML file
 load_repos_from_yaml() {
-      local repo_directory="repo-def"
+	local repo_directory="repo-def"
 
-    # Check if the DEFINITION variable is set and non-empty
-    if [[ -z "$DEFINITION" ]]; then
-        echo "The DEFINITION variable is not set. Please specify a YAML file name."
-        return 1
-    fi
+	# Check if the DEFINITION variable is set and non-empty
+	if [[ -z "$DEFINITION" ]]; then
+		echo "The DEFINITION variable is not set. Please specify a YAML file name."
+		return 1
+	fi
 
-    # Find the full path to the desired definition YAML file
-    local definition_file=$(find "$repo_directory" -type f -name "$DEFINITION.yaml" | head -n 1)
+	# Find the full path to the desired definition YAML file
+	local definition_file=$(find "$repo_directory" -type f -name "$DEFINITION.yaml" | head -n 1)
 
-    # Check if the definition file was found
-    if [[ -z "$definition_file" ]]; then
-        echo "No YAML file named $DEFINITION.yaml found in $repo_directory."
-        return 1
-    fi
+	# Check if the definition file was found
+	if [[ -z "$definition_file" ]]; then
+		echo "No YAML file named $DEFINITION.yaml found in $repo_directory."
+		return 1
+	fi
 
-    # Declare the global associative array
-    declare -g -A REPOS
+	# Declare the global associative array
+	declare -g -A REPOS
 
-    # Use yq to parse the YAML file and populate the REPOS array
-    # Make sure to use correct quotes and verify the command based on your yq version
-    while IFS=" " read -r key value; do
-        REPOS["$key"]="$value"
-    done < <(yq -r '. | to_entries[] | "\(.key) \(.value)"' "$definition_file")
+	tmp=$(yaml_load "$definition_file")
 
-    # Optionally: Display the loaded repository URLs
-    for repo in "${!REPOS[@]}"; do
-        echo "[\"$repo\"] = \"${REPOS[$repo]}\""
-    done
+	echo $tmp
+
+	for i in $tmp; do
+		# each $i has format reponame="URL"
+		key=$(echo "$i" | cut -d= -f1)               # Extract the key up to the '='
+		value=$(echo "$i" | cut -d= -f2 | tr -d '"') # Extract the value, remove enclosing quotes
+
+		REPOS["$key"]="$value"
+	done
+
+	#Optionally: Display the loaded repository URLs
+	for repo in "${!REPOS[@]}"; do
+		echo "[\"$repo\"] = \"${REPOS[$repo]}\""
+	done
 }
 
 load_repos_from_yaml
+
+exit
 
 echo "Syncing repositories..."
 
 mkdir -p "${REPOS_DIR}"
 
-if pushd "${REPOS_DIR}" &> /dev/null; then
-    for repo in "${!REPOS[@]}"; do
-        _dir="$repo"
-        _url="${REPOS[$repo]}"
+if pushd "${REPOS_DIR}" &>/dev/null; then
+	for repo in "${!REPOS[@]}"; do
+		_dir="$repo"
+		_url="${REPOS[$repo]}"
 
-        echo $_dir
-        if [ ! -d "$_dir" ]; then
-            git clone "$_url" "$_dir"
-        else
-            ( cd "$_dir" || exit; git fetch origin )
-        fi
-    done
-    popd &> /dev/null
+		echo $_dir
+		if [ ! -d "$_dir" ]; then
+			git clone "$_url" "$_dir"
+		else
+			(
+				cd "$_dir" || exit
+				git fetch origin
+			)
+		fi
+	done
+	popd &>/dev/null
 fi
